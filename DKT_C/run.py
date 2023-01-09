938fdc06c02519a510855cf31f6b9189453ac94f
xx <942076927@qq.com>
2023-01-09 17:08:58 +0800

    Merge remote-tracking branch 'origin/master'

--- DKT_C/run.py
+++ DKT_C/run.py
@@ -1,11 +1,10 @@
-# coding: utf-8
-# 2021/4/24 @ zengxiaonan
 import logging
 import numpy as np
 import torch
 import torch.utils.data as Data
+import random
 
-from DKT_C import DKT
+from DKT_C import DKT_Module
 
 NUM_QUESTIONS = 31
 BATCH_SIZE = 64
@@ -18,39 +17,44 @@ def get_data_loader(data_path, batch_size, shuffle=False):
     data_loader = Data.DataLoader(data, batch_size=batch_size, shuffle=shuffle)
     return data_loader
 
+data = torch.FloatTensor(np.load('../dataset/data.npy'))
+print(data.size())
 
-train_loader = get_data_loader('D:\Files\experiment\\20220305\\train_data.npy', BATCH_SIZE, True)
-test_loader = get_data_loader('D:\Files\experiment\\20220305\\test_data.npy', BATCH_SIZE, False)
-logging.getLogger().setLevel(logging.INFO)
+def Train_Test_Fold(data, batch_size, train_size=.7, shuffle=True):
+    if shuffle:
+        random.shuffle(data)
+    boundary = round(len(data) * train_size)
+    train_data = data[: boundary]
+    test_data = data[boundary:]
+    print(train_data)
+    print("train_data students num is:", train_data.shape[0])
+    print("train_data students num is:", test_data.shape[0])
+    train_loader = Data.DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
+    test_loader = Data.DataLoader(test_data, batch_size=batch_size, shuffle=shuffle)
+    return train_loader, test_loader
+n_splits = 5
+aucs = []
+for split in range(n_splits):
+    train_loader, test_loader = Train_Test_Fold(data, batch_size=1)
+    logging.getLogger().setLevel(logging.INFO)
+    dkt = DKT_Module.DKT(NUM_QUESTIONS, HIDDEN_SIZE, NUM_LAYERS)
+    train_skills_all, train_pred_all, train_truth_all = dkt.train(train_loader, epoch=200)
+    auc, eval_pred_all, eval_truth_all, eval_skills_all = dkt.eval(test_loader)
+    print("split:%d, auc: %.6f" % (split, auc))
+    aucs.append(auc)
 
-# 输出训练集和测试集学生数目
-train_data = np.load('D:\Files\experiment\\20220305\\train_data.npy')
-
-test_data = np.load('D:\Files\experiment\\20220305\\test_data.npy')
-print("\n=====================================================")
-print("train_data students num is:", train_data.shape[0])
-print("train_data students num is:", test_data.shape[0])
-print("=====================================================")
-
-
-dkt = DKT(NUM_QUESTIONS, HIDDEN_SIZE, NUM_LAYERS)
-train_skills_all, train_pred_all, train_truth_all = dkt.train(train_loader, epoch=1000)
 dkt.save("dkt.params")
 dkt.load("dkt.params")
-print("test_loader:", test_loader)
-auc, eval_pred_all, eval_truth_all, eval_skills_all = dkt.eval(test_loader)
-print("auc: %.6f" % auc)
+print(aucs)
 
-train_skills_data_path = 'D:\Files\experiment\\20220305\\train_skills_all.txt'
+train_skills_data_path = '../dataset/train_skills_all.txt'
 with open(train_skills_data_path, "w") as f_skills:
     f_skills.write(str(train_skills_all))
 
-
-train_pred_data_path = 'D:\Files\experiment\\20220305\\train_pred_all.txt'
+train_pred_data_path = '../dataset/train_pred_all.txt'
 with open(train_pred_data_path, "w") as f_pred:
     f_pred.write(str(train_pred_all))
 
-train_truth_data_path = 'D:\Files\experiment\\20220305\\train_truth_all.txt'
+train_truth_data_path = '../dataset/train_truth_all.txt'
 with open(train_truth_data_path, "w") as f_pred:
     f_pred.write(str(train_truth_all))
-
---
+++ FPGrowth_Frequency/Apriori.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from apyori import apriori
+
+
